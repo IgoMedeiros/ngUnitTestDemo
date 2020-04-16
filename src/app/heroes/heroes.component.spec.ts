df9da2a8ac6070b507f485446c2a2cce4e24cf71
IgoMedeiros <igopdm@gmail.com>
2020-04-16 17:23:41 -0300

    create heroesComponent test & add delete unittest

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,34 @@
+import { HeroesComponent } from "./heroes.component";
+import { of } from "rxjs";
+import { Hero } from "../hero";
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES: Hero[];
+  let mockHeroesService;
+
+  beforeEach(() => {
+    HEROES = [
+      {id: 1, name: 'SpiderDude', strength: 8},
+      {id: 2, name: 'Wonderful Woman', strength: 24},
+      {id: 3, name: 'SuperDude', strength: 55},
+    ];
+
+    mockHeroesService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+    component = new HeroesComponent(mockHeroesService);
+  });
+
+  describe('delete', () => {
+    it('should remove the indicated hero from the heroes list', () => {
+      mockHeroesService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      const heroes = HEROES.filter(h => h.id !== 2) as Hero[];
+
+      component.delete(HEROES[1]);
+
+      expect(component.heroes).toEqual(heroes);
+    });
+  });
+});
