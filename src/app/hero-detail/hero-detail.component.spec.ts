0f975af06db9772cf219818e7065ced6c0077497
IgoMedeiros <igopdm@gmail.com>
2020-04-18 11:25:04 -0300

    Create integration to HeroDetailComponent. - Jasmine Mocks - Verify template data.

---
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { HeroDetailComponent } from "./hero-detail.component";
+import { ActivatedRoute } from "@angular/router";
+import { HeroService } from "../hero.service";
+import { Location } from "@angular/common";
+import { of } from "rxjs";
+import { FormsModule } from "@angular/forms";
+
+export type Spide<T> = {
+  [Method in keyof T]: jasmine.Spy;
+};
+
+describe('HeroDetailComponent', () => {
+  let fixture: ComponentFixture<HeroDetailComponent>, mockActivatedRoute, mockHeroService: Spide<HeroService>, mockLocation;
+
+  beforeEach(() => {
+    mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero']);
+    mockLocation = jasmine.createSpyObj(['back']);
+    mockActivatedRoute = {
+      snapshot: { paramMap: { get: () => '1'}}
+    };
+
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [HeroDetailComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute},
+        { provide: Location, useValue: mockLocation},
+        { provide: HeroService, useValue: mockHeroService}
+      ]
+    });
+
+    fixture = TestBed.createComponent(HeroDetailComponent);
+    mockHeroService.getHero.and.returnValue(of({id: 2, name: 'SuperDude', strength: 100}));
+  });
+
+  it('should render hero name in a h2 tag', () => {
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SUPERDUDE');
+  });
+});
