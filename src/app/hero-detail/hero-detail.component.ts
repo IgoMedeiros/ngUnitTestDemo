3b5e30024db1362bb7a21cc64a46e12a06899d82
IgoMedeiros <igopdm@gmail.com>
2020-04-18 14:33:46 -0300

    Add integration test with Promise.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -33,12 +33,20 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
- save(): void {
+  save(): void {
+    var p = new Promise((resolve) => {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+      resolve();
+    });
+  }
+
+ /* save(): void {
   debounce(() => {
     this.heroService.updateHero(this.hero)
       .subscribe(() => this.goBack());
   }, 250, false)();
-  }
+  } */
 }
 
 function debounce(func, wait, immediate) {
